d6561e0189ad454a10e4da2387c128c9a3823b60
Michael Carroll <carrollm2@gmail.com>
2020-01-19 20:07:05 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -8,10 +8,3 @@ class CreateShows < ActiveRecord::Migration[5.2]
     end
   end
 end
-
-
-class AddSeasonToShows < ActiveRecord::Migration[5.2]
-  def change
-    add_column :shows, :season
-  end
-end
