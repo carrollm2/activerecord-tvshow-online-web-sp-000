5390a63f09844ff88542f659f73b4ce60deb2aef
Michael Carroll <carrollm2@gmail.com>
2020-01-19 20:24:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,12 +8,13 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     most_popular_show = Show.new
     Show.all.map do |s|
+      binding.pry
       if s.rating == Show.highest_rating
         most_popluar_show = s
       end
     end
     most_popular_show.name
-    binding.pry
+
   end
 
 end
